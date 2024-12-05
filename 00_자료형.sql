e1a4b0310857c83448e2549cf0d0893b493c0b6b
mochoping <hodolskm@gmail.com>
2024-12-05 16:48:04 +0900

    SELECT ORDER BY FUNCTION

---
+++ 00_자료형.sql
@@ -0,0 +1,24 @@
+/*
+1. 문자열 타입(Character Data Types)
+
+
+2. 숫자 데이터 타입(Numeric Data Types)
+
+3. 날짜 및 시간 데이터 타입(Date and Time Data Types)
+Date(n)  = 기본크기 날짜 시간 시 분 초 기본크기 7바이트
+4. 이진 데이터 타입(Binary Data Type)
+Blob  크기 제한이 없음
+이미지, 동영상 이외 여러 데이터 파일 저장할 수 있음
+5. 논리현 데이터 타입(Oracle에서만 사용 가능)
+
+
+*/
+/**
+order by 절
+기본적으로 오름차순 asc
+내림차순 desc
+
+null first / null last
+null 값을 어디에 배치할지 설정
+
+/
---
+++ 실습문제/01_select_practies orderby.sql
@@ -0,0 +1,13 @@
+SELECT * FROM EMPLOYEe;
+
+SELECT emp_name salary * 12 AS 연봉
+FROM EMPLOYEE 
+ORDER BY 
+
+SELECT emp_name, phone
+FROM EMPLOYEE
+order BY phone NULLS FIRST
+
+SELECT emp_name, phone
+FROM EMPLOYEE
+order BY phone NULLS LAST -- 이게 기본값.
