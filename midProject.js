a78ec4ae8c16d616e1fcf04953b2dcf63d398087
mochoping <hodolskm@gmail.com>
2024-12-18 17:50:37 +0900

    topbalance

---
+++ midProject.js
@@ -0,0 +1,9 @@
+userId;
+const expIdText = /^[A-Za-z가-힇]{4,20}$/; // 대소문자 구분할것인지?
+userPw;
+const expPwText =
+  /^(?=.*[a-z])(?=.*[A-Z])(?=.*[!@#$%^&*])[A-Za-z\d!@#$%^&*]{8,}$/;
+userPhone;
+const expHpText = /^\d{3}-\d{3,4}-\d{4}$/;
+userName;
+const expNameText = /^[가-힣]{2}$/;
